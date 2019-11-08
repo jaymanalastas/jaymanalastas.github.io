985b5e17fe0d3983490e627b7b308fbb9b561a0d
jaymanalastas <jay.manalastas@outlook.com>
2019-11-08 21:56:29 +0000

    using attila theme for blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'John Manalastas'
-SITENAME = "All Things Data"
+AUTHOR = u'John Manalastas'
+SITENAME = u"All Things Data"
+SITESUBTITLE = u'Blogging my way to data science'
 SITEURL = ''
 
 PATH = 'content'
-
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'En'
 
 # Feed generation is usually not desired when developing
@@ -18,6 +19,14 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+
+# Pagination: 
+DEFAULT_PAGINATION = 3
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -31,34 +40,111 @@ SOCIAL = (('github', 'https://github.com/jaymanalastas'),
 	  ('facebook','https://www.facebook.com/johnjay.manalastas'),
           ('linkedin', 'https://www.linkedin.com/in/jaymanalastas/'),)
 
-
-AUTHORS_BIO = {
-  "John Manalastas": {
-    "name": "John Manalastas",
-    "cover": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
-    "image": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
-    "LinkedIn": "https://www.linkedin.com/in/jaymanalastas/",
-    "location": "Orlando, FL",
-    "bio": "Analyst looking to leverage my passion for learning and analyzing trends to produce actionable analysis to executives."
-  }
-}
-
-
-PROFILE_IMAGE = "img/PROFILE.jpg"
+# PROFILE_IMAGE = "img/PROFILE.jpg"
 GITHUB_URL = ('https://github.com/jaymanalastas')
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
+#HOME_COLOR = 'black'
+#COLOR_SCHEME_CSS = 'darkly.css'
+STATIC_PATHS = ['assets']
 
-COLOR_SCHEME_CSS = 'monokai.css'
+EXTRA_PATH_METADATA = {
+    'assets/robots.txt': {'path': 'robots.txt'},
+    'assets/favicon.ico': {'path': 'favicon.ico'},
+    'assets/CNAME': {'path': 'CNAME'}
+}
 
-STATIC_PATHS = ['2019']
+
+
+## Post and Pages Path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+
+## Tags and Category path:
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+
 
 RELATIVE_URLS = False
-
 MARKUP = ('md', 'ipynb')
 
+
+
+#### Plugins
 PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['ipynb.markup']
+PLUGINS = ['sitemap', 'neighbors','ipynb.markup','assets']
+#'ipynb.markup', 'assets', 
+### Stiemap
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 THEME = "themes/attila"
+
+
+####
+# To set background image for the home page.
+HOME_COVER = 'https://unsplash.com/photos/TO2l2mxk5w0'
+#'https://casper.ghost.org/v1.0.0/images/welcome.jpg'
+
+
+# Custom Header
+
+HEADER_COVERS_BY_TAG = {'python-cloud':'https://casper.ghost.org/v1.0.0/images/writing.jpg'}
+
+AUTHORS_BIO = {
+  "john manalastas": {
+    "name": "John Manalastas",
+    "cover": "https://casper.ghost.org/v1.0.0/images/team.jpg",
+    "image": "assets/images/avatar.png",
+    "linkedin": "https://www.linkedin.com/in/jaymanalastas/",
+    "github": "https://github.com/jaymanalastas",
+    "location": "Orlando, FL",
+    "bio": "Finance leader looking to leverage my passion for learning and analyzing trends to produce actionable analysis to executives."
+
+  }
+}
+
+COLOR_SCHEME_CSS = 'github.css'
+
+CSS_OVERRIDE = ['assets/css/myblog.css']
+
+# Jinja config - Pelican 4
+JINJA_ENVIRONMENT = {
+  'extensions' :[
+    'jinja2.ext.loopcontrols',
+    'jinja2.ext.i18n',
+    'jinja2.ext.with_',
+    'jinja2.ext.do'
+  ]
+}
+
+JINJA_FILTERS = {'max': max}
+
